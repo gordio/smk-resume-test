524818595c2545e4cf7b7febaf77923106e60704
Oleg Gordienko <admin@gordio.pp.ua>
2014-06-05 01:48:16 +0300

    accounts: (fix) Reorder imports

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,9 +1,9 @@
 from django import forms
 from django.utils.translation import ugettext_lazy as _
-from userena.forms import SignupForm
-from captcha.fields import CaptchaField
 from django.core.validators import RegexValidator
 from django.core.exceptions import ValidationError
+from userena.forms import SignupForm
+from captcha.fields import CaptchaField
 
 
 class SignupFormExtra(SignupForm):
