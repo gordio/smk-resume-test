4c180b9354bf0459915212675be004a87f1cfebd
Oleg Gordienko <admin@gordio.pp.ua>
2014-06-04 23:20:25 +0300

    accounts: Add other field in SigIn form

--- accounts/forms.py
+++ accounts/forms.py
@@ -7,6 +7,20 @@ from django.core.exceptions import ValidationError
 
 
 class SignupFormExtra(SignupForm):
+    first_name = forms.CharField(label=_('First name'), max_length=30,
+        required=True)
+    last_name = forms.CharField(label=_('Last name'), max_length=30,
+        required=True)
+    burn_date = forms.DateField(label=_('Burn date'), required=True,
+        help_text="format: YEAR-MONTH-DAY")
+    mob_phone = forms.CharField(label=_('Phone'), required=True,
+        validators = [
+            RegexValidator(
+                r'^\([0-9]{3}\) [0-9]{3}-[0-9]{1,4}$',
+                message="Wrong format"
+            ),
+        ],
+        help_text='format: (099) 555-1234')
     captcha = CaptchaField()
 
     def clean_password1(self):
@@ -17,3 +31,14 @@ class SignupFormExtra(SignupForm):
         if len(password) > 16:
             raise ValidationError('Password too long')
         return password
+
+    def save(self):
+        new_user = super(SignupFormExtra, self).save()
+
+        new_user.first_name = self.cleaned_data['first_name']
+        new_user.last_name = self.cleaned_data['last_name']
+        new_user.burn_date = self.cleaned_data['burn_date']
+        new_user.mob_phone = self.cleaned_data['mob_phone']
+        new_user.save()
+
+        return new_user
