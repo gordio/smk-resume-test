5520a1d2a74dde7b29bbabbcb01c78b88f4c4b6e
Oleg Gordienko <admin@gordio.pp.ua>
2014-06-05 04:29:54 +0300

    accounts: Init (broken) tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from .forms import SignupFormExtra
 
-# Create your tests here.
+
+class SignInTests(TestCase):
+    """
+    User sign in form
+    """
+
+    def test_correct_data(self):
+        form_data = {
+            'username': "myusername",
+            'password1': "123456",
+            'password2': "123456",
+            'email': "user@mail.local",
+            'first_name': "FirstName",
+            'last_name': "LastName",
+            'burn_date': "1999-01-01",
+            'mob_phone': "(420) 555-1234",
+        }
+        form = SignupFormExtra(data=form_data)
+        # FIXME: Ignore captcha
+        self.assertTrue(form.is_valid())
+
+    def test_required_data(self):
+        # response = self.client.post("/accounts/signin", {'foo': 'bar'})
+        # self.assertFormError(response, 'form', 'foo', 'This field is required.')
+        pass
+
+
+    def test_error_data(self):
+        pass
